7bf668cdd807b5ad3261978b91b070f61310b68c
szp <twer@twers-MacBook-Pro-3.local>
2013-01-27 16:40:06 +0800

    implement output and its test

---
+++ src/main/java/ouput/MessageOutput.java
@@ -0,0 +1,7 @@
+package ouput;
+
+public class MessageOutput {
+    public void showText(String content) {
+        System.out.println(content);
+    }
+}
--- src/test/java/output/MessageOutputTest.java
+++ src/test/java/output/MessageOutputTest.java
@@ -1,4 +1,30 @@
 package output;
 
+import org.junit.Test;
+import ouput.MessageOutput;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class MessageOutputTest {
+    @Test
+    public void should_show_text_message_in_screen() {
+        try {
+            PrintStream standOut = System.out;
+            System.setOut(new PrintStream("/Users/twer/IdeaProjects/Rich/src/test/java/output/output"));
+            MessageOutput messageOutput = new MessageOutput();
+            messageOutput.showText("aaa");
+            FileInputStream fileInputStream = new FileInputStream("/Users/twer/IdeaProjects/Rich/src/test/java/output/output");
+            Scanner actual = new Scanner(fileInputStream);
+            assertThat(actual.next(), is("aaa"));
+            System.setOut(standOut);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
